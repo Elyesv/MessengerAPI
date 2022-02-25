0c923414212947f922097dde0b8a5c6625201a7a
Elyes <elyesvoisin52@gmail.com>
2022-02-25 12:11:55 +0100

    ADD header verification

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,8 +4,8 @@ module.exports = (req, res, next)=>{
     console.log('####################################################')
     console.log('Authentification en cours')
     const decodedToken = jsontoken.verify(req.headers.authorization.split(' ')[1], 'LeDMfrqJahLs74FR').userId
-    if(req.body.userId !== "null"){
-        if( req.body.userId && req.body.userId != decodedToken){
+    if(req.headers.userid !== "null"){
+        if( req.headers.userid && req.headers.userid != decodedToken){
             throw 'Wrong ID'
         }
         else{
